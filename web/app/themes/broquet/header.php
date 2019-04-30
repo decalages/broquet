476308d8d85ac4569f1a9ef94021da47c0925e45
Stéphane Broquet <stephane@broquet.ch>
2019-04-30 23:03:30 +0200

    first commit

--- web/app/themes/broquet/header.php
+++ web/app/themes/broquet/header.php
@@ -18,14 +18,14 @@
 	<link rel="profile" href="https://gmpg.org/xfn/11" />
 	<link rel="stylesheet" href="https://use.typekit.net/ojw6ats.css">
 	<?php wp_head(); ?>
-	<link rel="apple-touch-icon" sizes="180x180" href="/wp-content/themes/broquet/favicon/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="/wp-content/themes/broquet/favicon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="/wp-content/themes/broquet/favicon/favicon-16x16.png">
-	<link rel="manifest" href="/wp-content/themes/broquet/favicon/site.webmanifest">
-	<link rel="mask-icon" href="/wp-content/themes/broquet/favicon/safari-pinned-tab.svg" color="#343434">
-	<link rel="shortcut icon" href="/wp-content/themes/broquet/favicon/favicon.ico">
+	<link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon-16x16.png">
+	<link rel="manifest" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/site.webmanifest">
+	<link rel="mask-icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/safari-pinned-tab.svg" color="#343434">
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon/favicon.ico">
 	<meta name="msapplication-TileColor" content="#fbfbfb">
-	<meta name="msapplication-config" content="/wp-content/themes/broquet/favicon/browserconfig.xml">
+	<meta name="msapplication-config" content="<?php echo get_stylesheet_directory_uri(); ?>/favicon/browserconfig.xml">
 	<meta name="theme-color" content="#ffffff">
 	<?php
     $overlaycolor = get_field("overlay-color");
